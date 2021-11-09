e9bb98a3ec7c51e84e4190ea7d40c1224f007822
Mass23 <massimo.bourquin@gmail.com>
2021-11-09 14:34:42 +0100

    dn_genomes

--- workflow/scripts/dn_genomes_script.py
+++ workflow/scripts/dn_genomes_script.py
@@ -2,16 +2,15 @@
 import os
 import subprocess
 
-acc_file = snakemake.input[0]
-genus=acc_file.split('/')[-2]
-
-for line in open(acc_file, 'r').read().split('\n'):
-    if line.startswith('GCA'):
-        args = ['ncbi-genome-download','-o',os.path.join(snakemake.RESULTS_DIR, 'Genomes', genus),'--formats','fasta','--flat-output','-s','genbank','-A',line,'bacteria']     
-        print(' '.join(args))
-        subprocess.call(' '.join(args), shell = True)
-    elif line.startswith('GCF'):
-        args = ['ncbi-genome-download','-o',os.path.join(snakemake.RESULTS_DIR, 'Genomes', genus),'--formats','fasta','--flat-output','-s','refseq','-A',line,'bacteria']
-        subprocess.call(' '.join(args), shell = True)
-    else:
-        print(str(line) + ' is not a refseq or genbank accession!')
+for acc_file in snakemake.input[0]:
+    genus=acc_file.split('/')[-2]
+    for line in open(acc_file, 'r').read().split('\n'):
+        if line.startswith('GCA'):
+            args = ['ncbi-genome-download','-o',os.path.join(snakemake.RESULTS_DIR, 'Genomes', genus),'--formats','fasta','--flat-output','-s','genbank','-A',line,'bacteria']     
+            print(' '.join(args))
+            subprocess.call(' '.join(args), shell = True)
+        elif line.startswith('GCF'):
+            args = ['ncbi-genome-download','-o',os.path.join(snakemake.RESULTS_DIR, 'Genomes', genus),'--formats','fasta','--flat-output','-s','refseq','-A',line,'bacteria']
+            subprocess.call(' '.join(args), shell = True)
+        else:
+            print(str(line) + ' is not a refseq or genbank accession!')
