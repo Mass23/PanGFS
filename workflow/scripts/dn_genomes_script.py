aa0dd8569b4b7c3e223a29674364279138f0d4a4
Mass23 <massimo.bourquin@gmail.com>
2021-11-09 15:03:38 +0100

    dn_genomes

--- workflow/scripts/dn_genomes_script.py
+++ workflow/scripts/dn_genomes_script.py
@@ -3,8 +3,10 @@ import os
 import subprocess
 
 for acc_file in snakemake.input[0]:
+    print(acc_file)
     genus=acc_file.split('/')[-2]
     for line in open(acc_file, 'r').read().split('\n'):
+        print(line)
         if line.startswith('GCA'):
             args = ['ncbi-genome-download','-o',snakemake.output[0],'--formats','fasta','--flat-output','-s','genbank','-A',line,'bacteria']     
             subprocess.call(' '.join(args), shell = True)
