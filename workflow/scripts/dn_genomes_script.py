3dc1fe1f5b4fe580287ca89a7f713397eeab14c8
Mass23 <massimo.bourquin@gmail.com>
2021-11-09 15:02:23 +0100

    dn_genomes

--- workflow/scripts/dn_genomes_script.py
+++ workflow/scripts/dn_genomes_script.py
@@ -6,10 +6,10 @@ for acc_file in snakemake.input[0]:
     genus=acc_file.split('/')[-2]
     for line in open(acc_file, 'r').read().split('\n'):
         if line.startswith('GCA'):
-            args = ['ncbi-genome-download','-o',snakemake.output[0], 'Genomes', genus),'--formats','fasta','--flat-output','-s','genbank','-A',line,'bacteria']     
+            args = ['ncbi-genome-download','-o',snakemake.output[0],'--formats','fasta','--flat-output','-s','genbank','-A',line,'bacteria']     
             subprocess.call(' '.join(args), shell = True)
         elif line.startswith('GCF'):
-            args = ['ncbi-genome-download','-o',snakemake.output[0], 'Genomes', genus),'--formats','fasta','--flat-output','-s','refseq','-A',line,'bacteria']
+            args = ['ncbi-genome-download','-o',snakemake.output[0],'--formats','fasta','--flat-output','-s','refseq','-A',line,'bacteria']
             subprocess.call(' '.join(args), shell = True)
         else:
             print(str(line) + ' is not a refseq or genbank accession!')
