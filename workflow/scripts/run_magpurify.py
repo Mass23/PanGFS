642f35b6b10077dcf06d2f5389f06173bb8a78b8
Mass23 <massimo.bourquin@gmail.com>
2021-11-30 14:50:08 +0100

    fixing magpurify

--- workflow/scripts/run_magpurify.py
+++ workflow/scripts/run_magpurify.py
@@ -8,21 +8,24 @@ def process_genus(in_folder, out_folder):
         mag_in = os.path.join(in_folder, raw_mags[mag])
         mag_puri_out = os.path.join(in_folder, raw_mags[mag].replace('.fasta','_magpurify/'))
         mag_out = os.path.join(out_folder, clean_mags[mag])
-        args1 = ['magpurify', 'phylo-markers', mag_in, mag_puri_out]
+        args1 = ['magpurify', 'phylo-markers', 
+        '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
         subprocess.call(' '.join(args1), shell = True)
-        args2 = ['magpurify', 'clade-markers', mag_in, mag_puri_out]
+        args2 = ['magpurify', 'clade-markers', 
+        '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
         subprocess.call(' '.join(args2), shell = True)
-        args3 = ['magpurify', 'tetra-freq',    mag_in, mag_puri_out]
+        args3 = ['magpurify', 'tetra-freq',    
+        '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
         subprocess.call(' '.join(args3), shell = True)
-        args4 = ['magpurify', 'gc-content',    mag_in, mag_puri_out]
+        args4 = ['magpurify', 'gc-content',    
+        '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
         subprocess.call(' '.join(args4), shell = True)
-        args5 = ['magpurify', 'known-contam',  mag_in, mag_puri_out]
+        args5 = ['magpurify', 'known-contam',  
+        '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
         subprocess.call(' '.join(args5), shell = True)
-        args_clean = ['magpurify', 'clean-bin', mag_in, mag_puri_out, mag_out]
+        args_clean = ['magpurify', 'clean-bin', 
+        '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out, mag_out]
         subprocess.call(' '.join(args_clean), shell = True)
-
-
-os.system('export MAGPURIFYDB=/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0')
 
 for i in range(0,len(snakemake.input)):
     process_genus(snakemake.input[i], snakemake.output[i])
