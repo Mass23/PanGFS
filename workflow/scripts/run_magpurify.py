300c26ebe40eb1926cc67c5af975a3c403e2e018
Mass23 <massimo.bourquin@gmail.com>
2021-11-30 14:43:28 +0100

    added magpurify export db

--- workflow/scripts/run_magpurify.py
+++ workflow/scripts/run_magpurify.py
@@ -1,3 +1,5 @@
+import os
+
 def process_genus(in_folder, out_folder):
     raw_mags = glob.glob(in_folder)
     clean_mags = [i.replace('.fasta','_clean.fasta') for i in raw_mags]
@@ -17,6 +19,7 @@ def process_genus(in_folder, out_folder):
         subprocess.call(' '.join(args5), shell = True)
         args_clean = ['magpurify', 'clean-bin', mag_in, mag_puri_out, mag_out]
         subprocess.call(' '.join(args6), shell = True)
+
 
 os.system('export MAGPURIFYDB=/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0')
 
