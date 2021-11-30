a8a03e3fec25575850d0438749f385af2d3d4827
Mass23 <massimo.bourquin@gmail.com>
2021-11-30 15:08:48 +0100

    fixing magpurify

--- workflow/scripts/run_magpurify.py
+++ workflow/scripts/run_magpurify.py
@@ -14,17 +14,14 @@ def process_genus(in_folder, out_folder):
         args2 = ['magpurify', 'clade-markers',
         '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
         subprocess.call(' '.join(args2), shell = True)
-        args3 = ['magpurify', 'tetra-freq',    
-        '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
+        args3 = ['magpurify', 'tetra-freq', mag_in, mag_puri_out]
         subprocess.call(' '.join(args3), shell = True)
-        args4 = ['magpurify', 'gc-content',    
-        '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
+        args4 = ['magpurify', 'gc-content', mag_in, mag_puri_out]
         subprocess.call(' '.join(args4), shell = True)
         args5 = ['magpurify', 'known-contam',
         '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
         subprocess.call(' '.join(args5), shell = True)
-        args_clean = ['magpurify', 'clean-bin', 
-        '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out, mag_out]
+        args_clean = ['magpurify', 'clean-bin', mag_in, mag_puri_out, mag_out]
         subprocess.call(' '.join(args_clean), shell = True)
 
 for i in range(0,len(snakemake.input)):
