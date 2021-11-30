5f4dc2ef493cb944fb5f2b58bf5138b20604b58f
Mass23 <massimo.bourquin@gmail.com>
2021-11-30 14:57:34 +0100

    fixing magpurify

--- workflow/scripts/run_magpurify.py
+++ workflow/scripts/run_magpurify.py
@@ -8,10 +8,10 @@ def process_genus(in_folder, out_folder):
         mag_in = os.path.join(in_folder, raw_mags[mag])
         mag_puri_out = os.path.join(in_folder, raw_mags[mag].replace('.fasta','_magpurify/'))
         mag_out = os.path.join(out_folder, clean_mags[mag])
-        args1 = ['magpurify', 'phylo-markers', 
+        args1 = ['magpurify', 'phylo-markers', '--threads', '32',
         '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
         subprocess.call(' '.join(args1), shell = True)
-        args2 = ['magpurify', 'clade-markers', 
+        args2 = ['magpurify', 'clade-markers', '--threads', '32',
         '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
         subprocess.call(' '.join(args2), shell = True)
         args3 = ['magpurify', 'tetra-freq',    
@@ -20,7 +20,7 @@ def process_genus(in_folder, out_folder):
         args4 = ['magpurify', 'gc-content',    
         '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
         subprocess.call(' '.join(args4), shell = True)
-        args5 = ['magpurify', 'known-contam',  
+        args5 = ['magpurify', 'known-contam',  '--threads', '32',
         '--db', '/mnt/esb-storage-01/NOMIS/databases/MAGpurify-db-v1.0', mag_in, mag_puri_out]
         subprocess.call(' '.join(args5), shell = True)
         args_clean = ['magpurify', 'clean-bin', 
